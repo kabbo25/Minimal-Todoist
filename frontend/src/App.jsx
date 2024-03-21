44015149a8f00d8ebc797d5f4ec53cfd3e3b358d
kabbo25 <kabbo25@gmail.com>
2024-03-21 12:58:05 +0600

    going to previous

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,23 @@
 import "./styles/bootstrap.min.css";
 import "./styles/style.css";
-import {Route, Routes} from "react-router-dom";
-import {Signup} from "./pages/signup";
-import {Login} from "./pages/login";
-import {Todo} from "./pages/Todo/todo";
-import {NoRoute} from "./pages/404";
+import { Routes, Route } from "react-router-dom";
+import { Signup } from "./pages/signup";
+import { Login } from "./pages/login";
+import { Home } from "./pages/home";
+import { Todo } from "./pages/todo";
+import { NoRoute } from "./pages/404";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
-import {ChakraProvider} from '@chakra-ui/react'
-
 function App() {
     return (
-        <ChakraProvider>
-            <>
-                <Routes>
-                    <Route element={<Signup/>} path="/signup"></Route>
-                    <Route element={<Login/>} path="/login"></Route>
-                    <Route element={<Todo/>} path="/"></Route>
-                    <Route element={<NoRoute/>} path="*"></Route>
-                </Routes>
-            </>
-        </ChakraProvider>
+        <div>
+            <Routes>
+                <Route element={<Signup />} path="/signup"></Route>
+                <Route element={<Login />} path="/login"></Route>
+                <Route element={<Todo />} path="/"></Route>
+                <Route element={<NoRoute />} path="*"></Route>
+            </Routes>
+        </div>
     );
 }
-
+// go to previous state
 export default App;
